cc1d8af5c9b702dc2872930da174d7b2852588f9
alexbotha <alexbotha@hotmail.co.uk>
2022-07-22 20:21:23 +0100

    commit tree 8e31d2bd61ad2721e870471cd8f983526e628983

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+let [bessie, ,dolly, babe, little] = farmAnimals.split(' ')
+let [blackAndWhite, , black, pink] = farmAnimals.split(' ')
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red,orange,yellow,green,blue,indigo,violet] = colors
+const [r,o,y,g,b, ,v] = colors
+const[, , , , , indg, ] = colors
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +16,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+let {muppetName, color, song, job, partner} = muppet
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +33,9 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+let{song2,song4,} = nestedMuppet.album.theMuppetMovie
+let{nestedJob, nestedPartner} = nestedMuppet
 
 // Strings
 
